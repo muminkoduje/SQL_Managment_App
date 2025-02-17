40f827e08f7ec8c14897e9e13d7365d0296d83a1
mumionek <towcaleniejest2konto@gmail.com>
2025-02-17 16:50:17 +0100

    0. Project creation and setting up the environment

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import sqlalchemy as sql
+import tkinter as tk
+
+window = tk.Tk()
+window.title("SQL Managment App")
+window.geometry("1280x720")
+window.configure(bg='#333')
+
+window.mainloop()
+
